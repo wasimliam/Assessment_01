2af3412fecd625451919d8d10983f952fc028e70
wasimliam <liamwasim@gmail.com>
2024-04-01 21:26:32 +0600

    first commit

---
+++ calculator/main.js
@@ -0,0 +1,43 @@
+
+function Calculate() {
+  let first = parseInt(document.getElementById('selector1').value);
+  let sec = parseInt(document.getElementById('selector2').value);
+  let op = document.getElementById('mathValue').value;
+  let result;
+
+  if (document.getElementById("selector1").value.trim() == "") {
+    alert("Please Enter First Number!");
+    document.getElementById("selector1").focus();
+    return false;
+  }
+
+  if (document.getElementById("selector2").value.trim() == "") {
+    alert("Please Enter Second Number!");
+    document.getElementById("selector2").focus();
+    return false;
+  }
+
+  if (op == '+') {
+    result = first + sec;
+  } else if (op == '-') {
+    result = first - sec;
+  } else if (op == '*') {
+    result = first * sec;
+    op = "x"
+  } else if (op == '/') {
+    result = first / sec;
+    op = "÷";
+  }
+
+  document.getElementById('result').innerHTML = `${first} ${op} ${sec} = ${result}`;
+}
+
+
+function Reset()
+{
+  document.querySelector('#selector1').value = '';
+  document.querySelector('#mathValue').value = '0';
+  document.querySelector('#selector2').value = '';
+  document.querySelector('#result').innerHTML = '';
+
+}
